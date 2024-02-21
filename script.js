9db0b4717c92ea1bd3b220ac932fb85f43ede93a
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-02-20 20:19:39 -0500

    Change js code so that only one eye is visible
    This was done as a matter of aesthetic. Upon looking at the two eyes open, it felt redundant to keep them both open. Therefore, I
    resoned that a simple single eye icon that when clicked toggles both the type and visibilty of passwords was sufficient

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 // this will call to the togglePassword id and be run through an event listener to check if the input type should
 // be 'text' or 'password'
 const toggle=document.getElementById('togglePassword');
-const confirmToggle=document.getElementById('confirmToggle');
+// const confirmToggle=document.getElementById('confirmToggle');
 const password=document.querySelector('#user_password');
 const confirm=document.querySelector('#confirm_password');
 
 togglePassword.addEventListener('click', ()=>showPassword());
-confirmToggle.addEventListener('click', ()=>showPassword());
+// confirmToggle.addEventListener('click', ()=>showPassword());
 
 function showPassword(){
     // toggle the type attribut for user password
@@ -16,12 +16,12 @@ function showPassword(){
     password.setAttribute('type', type);
     confirm.setAttribute('type', type);
     // toggle the eye icon
-    if(togglePassword.src.match("content/eyeClosed.png")&&confirmToggle.src.match("content/eyeClosed.png")){
-         togglePassword.src="content/eyeOpen.png";
-        confirmToggle.src="content/eyeOpen.png";
-    } else if(togglePassword.src.match("content/eyeOpen.png")&&confirmToggle.src.match("content/eyeOpen.png")){
-         togglePassword.src="content/eyeClosed.png";
-        confirmToggle.src="content/eyeClosed.png";
+    if(togglePassword.src.match("content/eyeClosed.png")){
+        togglePassword.src="content/eyeOpen.png";
+        // confirmToggle.src="content/eyeOpen.png";
+    } else if(togglePassword.src.match("content/eyeOpen.png")){
+        togglePassword.src="content/eyeClosed.png";
+        // confirmToggle.src="content/eyeClosed.png";
     }   
 }
 
