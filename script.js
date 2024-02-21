10cd1cfd3aa42749006676fc155e14e2e1019316
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-02-20 22:30:32 -0500

    Add regex check for passwords
    if this fails, a message will appear explaining what the minimum requirements are.

--- script.js
+++ script.js
@@ -4,13 +4,28 @@
 // be 'text' or 'password'
 const toggle=document.getElementById('togglePassword');
 // const confirmToggle=document.getElementById('confirmToggle');
-const password=document.querySelector('#user_password');
-const confirm=document.querySelector('#confirm_password');
+const password=document.getElementById('user_password');
+const confirm=document.getElementById('confirm_password');
 
+// add regex for testing
+const regex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+// varable for flags around password
+var message=document.getElementById('match-pass')
 // Adds an event listener to the button, if clicked and somthing is invalid a flag will be raised
 const button=document.querySelector('button').addEventListener('click', function (e){
     //This will allow our passwords red border to flare if the information is invalid
     document.querySelector('form').classList.add('submitted')
+    if(password.value!==confirm.value){
+        message.style.visibility='visible';
+        message.textContent="*passwords do not match";
+    }else if(!regex.test(password.textContent)||!regex.test(confirm.textContent)){
+        message.style.visibility='visible';
+        message.textContent=
+        "*Passowrd must be 8 characters long with 1 capitol,"+ 
+        "1 lowercase, 1 number and 1 special character(!@#$%^&*)";
+    }
+    
 });
 
 togglePassword.addEventListener('click', ()=>showPassword());
